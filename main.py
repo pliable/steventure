fc567dcfec6fbcaecbd9e9a9ac778a70ecfcc8d7
Steve Choo <pliable@gmail.com>
2013-11-03 01:08:23 -0800

    commit main

---
+++ main.py
@@ -0,0 +1,10 @@
+__author__ = 'pliable'
+
+class steventure:
+   """Main entry class for steventure"""
+
+   def main(self):
+      print "hi  main"
+
+if __name__ == '__main__':
+   steventure.main()
